ae4520f1152238377537e399bfbbde72ffbef1af
ankush2311025 <ankson1010@gmail.com>
2024-11-15 10:17:43 +0530

    extra comment removed

--- models/User.js
+++ models/User.js
@@ -22,29 +22,3 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 export default User;
 
-// import { mongoose } from "mongoose";
-// const userSchema = new mongoose.Schema({
-//     email : {
-//     type :  String,
-//     required: true,
-//     unique : true
-// },
-// password:{
-//     type: String,
-// },
-// name:{
-//     type:String,
-//     required : true
-    
-// },
-// is_verified: {
-//     type: Boolean
-// },
-// bookedSeats:{
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'Seat'
-// }
-// });
-
-// const User = mongoose.model('User', userSchema);
-// export default User; 
